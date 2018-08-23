094bb3f0d41d4c456da5ee6295a2d5893795aec9
joney-wang <15829538541@163.com>
2018-08-23 22:00:00 +0800

    练习提交

--- src/main/java/com/joney/controller/UserController.java
+++ src/main/java/com/joney/controller/UserController.java
@@ -2,8 +2,11 @@ package com.joney.controller;
 
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +17,8 @@ import com.joney.pojo.User;
 @RestController
 @RequestMapping("/v2")
 public class UserController {
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private Resource resource;
@@ -37,6 +42,13 @@ public class UserController {
 //		r.setWebsite(resource.getWebsite());
 		BeanUtils.copyProperties(resource, r);
 		return Response.ok(r);
+	}
+	
+	@PostMapping("/postform")
+	public Response postTest(User u){
+		System.out.println(u.getName());
+		LOGGER.info(u.getName());
+		return Response.ok(u);
 	}
 	
 }
